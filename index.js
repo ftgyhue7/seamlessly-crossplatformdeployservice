f910a0d05695fe43f16b1af0606d78c73c77a9e3
ftgyhue7 <gs1WWWFISHY@hotmail.com>
2024-05-09 10:20:19 +0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const colors = ["red", "green", "blue"];
-const randomColor = colors[Math.floor(Math.random() * colors.length)];
-console.log(randomColor);
+function deserialize(data) {
+  if (!data.length) return null;
+  const root = new TreeNode(data.shift());
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    const leftVal = data.shift();
+    if (leftVal !== undefined) {
+      node.left = new TreeNode(leftVal);
+      queue.push(node.left);
+    }
+    const rightVal = data.shift();
+    if (rightVal !== undefined) {
+      node.right = new TreeNode(rightVal);
+      queue.push(node.right);
+    }
+  }
+  return root;
+}
