d6e7009ac29ebd00942562cf1f27a097e921c8ff
ftgyhue7 <gs1WWWFISHY@hotmail.com>
2024-05-17 23:51:57 +0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
+function serialize(root) {
+  const result = [];
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.shift();
+    if (node) {
+      result.push(node.val);
+      queue.push(node.left, node.right);
+    } else {
+      result.push(null);
+    }
+  }
+  while (result[result.length - 1] === null) {
+    result.pop();
+  }
+  return result;
+}
